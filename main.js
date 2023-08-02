72ec3f9803844a51640b6560ffee3c3782380f89
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-08-02 15:48:48 +0300

    done with core functionalities and basic gui

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let cityName = "kigali";
 let insertedCity = document.querySelector("#cityName");
 let submitBtn = document.querySelector("#submitBtn");
 let weatherInformation;
-
+let country = document.querySelector(".country");
+let cityLabel = document.querySelector(".city");
+let localtime = document.querySelector(".localtime");
+let tempC = document.querySelector(".temp-c");
+let tempF = document.querySelector(".temp-f");
 //submit phone
 submitBtn.addEventListener("click", function () {
   weatherInformation = getData(insertedCity.value);
-  console.log(weatherInformation);
 });
 
 // function to fetch weather info based on the location
@@ -17,6 +20,13 @@ async function getData(city) {
       { mode: "cors" }
     );
     const weatherInfo = await result.json();
+    const { location, current } = weatherInfo;
+    // assigning labels to their corresponding info
+    country.textContent += ` ${location.country}`;
+    cityLabel.textContent += ` ${location.name}`;
+    localtime.textContent += ` ${location.localtime}`;
+    tempC.textContent += ` ${current.temp_c}`;
+    tempF.textContent += ` ${current.temp_f}`;
     console.log(weatherInfo);
   } catch (error) {
     console.log(error);
