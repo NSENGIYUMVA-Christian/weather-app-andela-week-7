2ae95e509be21e668fa3ea1ca6e229ea8a0c4582
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-08-02 15:13:07 +0300

    done with fetching weather info based on inserted city

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("js");
+let cityName = "kigali";
+let insertedCity = document.querySelector("#cityName");
+let submitBtn = document.querySelector("#submitBtn");
+let weatherInformation;
+
+//submit phone
+submitBtn.addEventListener("click", function () {
+  weatherInformation = getData(insertedCity.value);
+  console.log(weatherInformation);
+});
+
+// function to fetch weather info based on the location
+async function getData(city) {
+  try {
+    const result = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=bd38e0750b764db6a8795807230208&q=${city}`,
+      { mode: "cors" }
+    );
+    const weatherInfo = await result.json();
+    console.log(weatherInfo);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//getData(cityName);
